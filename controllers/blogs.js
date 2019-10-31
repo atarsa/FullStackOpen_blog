cfaf2ca4ab79c751ae8f481ab4a9a0267923a8e2
ATarsa <agnieszkatarsa@gmail.com>
2019-10-31 17:41:56 +0000

    Exercise 4.8 Test getting all blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,12 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+   
+  if(blogs){
+    response.send(blogs)
+  }
 })
 
 blogsRouter.post('/', (request, response) => {
