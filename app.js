f2013cb1bf76baef8b1c0a656e3f7617f8e89934
ATarsa <agnieszkatarsa@gmail.com>
2019-11-04 20:33:16 +0000

    4.15 implement adding new users

--- app.js
+++ app.js
@@ -4,28 +4,31 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const blogsRouter = require('./controllers/blogs')
-// const middleware = require('./utils/middleware)
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
-console.log('connecting to', config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI);
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
+    logger.info('Connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connection to MongoDB', error.message);
+    logger.error('error connection to MongoDB', error.message);
   })
 
 
 app.use(cors())
 //app.use(express.static('build'))
 app.use(bodyParser.json())
-//app.use(middleware.requestLogger)
+app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
-// app.use(middleware.unknownEndpoint)
-// app.use(middleware.errorHandler)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
